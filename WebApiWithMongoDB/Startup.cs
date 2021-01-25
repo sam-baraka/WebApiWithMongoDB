50657137226dd3223f48873df659baef9a13b1e0
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-01-25 15:43:21 +0300

    Optimized usings

--- WebApiWithMongoDB/Startup.cs
+++ WebApiWithMongoDB/Startup.cs
@@ -1,20 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using WebApiWithMongoDB.Models;
 
-namespace WebApiWithMongoDB
+namespace WebApiWithMongoDBs
 {
     public class Startup
     {
