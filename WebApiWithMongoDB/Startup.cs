f350824e7727bbe1792637c3a504776c46b52e56
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-01-25 15:42:09 +0300

    Services confguration

--- WebApiWithMongoDB/Startup.cs
+++ WebApiWithMongoDB/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApiWithMongoDB.Models;
 
 namespace WebApiWithMongoDB
 {
@@ -26,7 +28,12 @@ namespace WebApiWithMongoDB
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // requires using Microsoft.Extensions.Options
+            services.Configure<BookstoreDatabaseSettings>(
+                Configuration.GetSection(nameof(BookstoreDatabaseSettings)));
 
+            services.AddSingleton<IBookstoreDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<BookstoreDatabaseSettings>>().Value);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
